b795fdad6ecf4f1362cf7f0e70b088d88060f09c
Muhammad Naveed <mnaveed.ashraf@systemsltd.com>
2023-10-17 16:53:31 +0500

    animationAddition

--- app/src/main/java/com/droidgeeks/slweatherapp/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/droidgeeks/slweatherapp/presentation/splash/SplashScreen.kt
@@ -1,7 +1,7 @@
 package com.droidgeeks.slweatherapp.presentation.splash
 
-import android.view.RoundedCorner
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
@@ -12,117 +12,135 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Shapes
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.droidgeeks.coreui.ui.theme.WeatherAppTheme
 import com.droidgeeks.slweatherapp.R
 import kotlin.random.Random
 
 @Composable
 fun SplashScreen() {
-    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        colorResource(id = R.color.dark_blue),
-                        colorResource(id = R.color.light_blue),
-                    ),
+    WeatherAppTheme() {
+        Scaffold(modifier = Modifier) {padding ->
+            Background(modifier = Modifier.padding())
+            val screenWidth = LocalConfiguration.current.screenWidthDp.dp+ 50.dp
+            val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+            val animatedValue = remember { Animatable(initialValue = screenWidth.value) }
+            LaunchedEffect(Unit) {
+                animatedValue.animateTo(
+                    targetValue = 0f,
+                    animationSpec = tween(durationMillis = 2000)
                 )
-            )
-    ) {
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            drawStars(this, screenWidth.value, screenHeight.value)
+            }
+            Foreground(animatedValue)
         }
-        Image(painter = painterResource(id = R.drawable.clouds),
+    }
+}
+
+@Composable
+fun Foreground(animatedValue: Animatable<Float, AnimationVector1D>) {
+
+    Box(modifier = Modifier.fillMaxSize()
+        .graphicsLayer {
+            this.translationX
+        }
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.clouds),
             modifier = Modifier
                 .width(180.dp)
                 .height(180.dp)
                 .alpha(1f)
-                .align(Alignment.TopCenter),
+                .align(Alignment.TopCenter)
+                .offset(x = animatedValue.value.dp),
             contentScale = ContentScale.Fit,
-            contentDescription = null)
-        Image(painter = painterResource(id = R.drawable.clouds),
+            contentDescription = null
+        )
+        Image(
+            painter = painterResource(id = R.drawable.clouds),
             modifier = Modifier
-                .padding(top = 200.dp)
+                .padding(top = 180.dp)
                 .width(120.dp)
                 .height(120.dp)
-                .offset(x = 10.dp)
+                .offset(x = 10.dp + animatedValue.value.dp)
                 .align(Alignment.TopEnd),
             contentDescription = "cloud"
         )
-        Image(painter = painterResource(id = R.drawable.clouds),
+        Image(
+            painter = painterResource(id = R.drawable.clouds),
             modifier = Modifier
-                .padding(top = 200.dp)
+                .padding(top = 180.dp)
                 .width(150.dp)
                 .height(150.dp)
-                .offset(x = (-50).dp)
+                .offset(x = (-50).dp + animatedValue.value.dp)
                 .align(Alignment.TopStart),
             contentDescription = "cloud"
         )
-        Image(painter = painterResource(id = R.drawable.clouds),
+        Image(
+            painter = painterResource(id = R.drawable.clouds),
             modifier = Modifier
                 .padding(top = 0.dp)
                 .width(280.dp)
                 .height(280.dp)
-                .align(Alignment.Center),
+                .align(Alignment.Center)
+                .offset(x = animatedValue.value.dp),
             contentDescription = "cloud"
         )
         Column(modifier = Modifier
             .align(Alignment.BottomCenter)
             .wrapContentSize()
+            .padding(bottom = 100.dp)
         ) {
 
-            Text(text = "",
-                modifier = Modifier,
+            Text(text = "Weather",
+                modifier = Modifier
+                    .offset(y = (10).dp)
+                    .align(Alignment.CenterHorizontally),
                 fontStyle = FontStyle.Normal,
-                style = MaterialTheme.typography.h1.copy(fontWeight = FontWeight(20))
+                fontSize = 58.sp,
+
+            )
+            Text(text = "Forecasts",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 30.dp),
+                fontStyle = FontStyle.Normal,
+                fontSize = 58.sp,
             )
             Button(
                 onClick = { /*TODO*/ },
                 colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primaryVariant),
                 shape = RoundedCornerShape(30.dp),
                 modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
                     .height(58.dp)
-                    .offset(y = (-100).dp)
             ) {
-                Text(text = "Get Started", modifier = Modifier.padding(horizontal = 46.dp))
+                Text(text = "Get Started", modifier = Modifier.padding(horizontal = 52.dp))
             }
 
         }
@@ -145,10 +163,48 @@ private fun drawStars(drawScope: DrawScope, screenWidth: Float, screenHeight: Fl
     }
 }
 
+@Composable
+fun Background(modifier: Modifier = Modifier){
+//    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+//    val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        colorResource(id = R.color.dark_blue),
+                        colorResource(id = R.color.light_blue),
+                    ),
+                )
+            )
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            drawStars(this, 500f, 500f)
+        }
+    }
+}
+
 @Preview
 @Composable
-fun SplashScreenPreview(){
+fun BackgroundPreview(){
     WeatherAppTheme {
-        SplashScreen()
+        Background()
     }
+}
+@Preview
+@Composable
+fun ForegroundPreview(){
+    WeatherAppTheme {
+//        Foreground(animatedValue)
+    }
+}
+@Preview
+@Composable
+fun SplashScreenPReview(){
+    SplashScreen()
 }
